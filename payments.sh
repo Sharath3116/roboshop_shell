abe9e7f20d43bcb0e9d70012ee820fa9c93bcd08
Sharath3116 <sharath3192@gmail.com>
2025-05-04 13:07:26 +0530

    new script

--- payments.sh
+++ payments.sh
@@ -30,3 +30,42 @@ fi
 
 dnf install python3.11 gcc python3-devel -y &>> $LOGFILE
     VALIDATE $? "python package install"
+
+id roboshop &>> $LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "Adding roboshop user"
+else
+    echo -e "roboshop user already exist...$Y SKIPPING $N"
+fi
+
+rm -rf /app &>> $LOGFILE
+VALIDATE $? "clean up existing directory"
+
+mkdir -p /app &>> $LOGFILE
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>> $LOGFILE
+VALIDATE $? "Downloading shipping application"
+
+cd /app  &>> $LOGFILE
+VALIDATE $? "Moving to app directory"
+
+unzip /tmp/payment.zip &>> $LOGFILE
+VALIDATE $? "Extracting payment application"
+
+pip3.11 install -r requirements.txt &>> $LOGFILE
+VALIDATE $? "Installing dependencies"
+
+cp /home/ec2-user/roboshop_shell/payment.service /etc/systemd/system/payment.service &>> $LOGFILE
+VALIDATE $? "Copying payment service"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable payment &>> $LOGFILE
+VALIDATE $? "Enable payment"
+
+systemctl start payment &>> $LOGFILE
+VALIDATE $? "Start payment"
