688919e728ce1d1d11e610e06766e601504fdd4f
Sharath3116 <sharath3192@gmail.com>
2025-05-04 12:50:35 +0530

    new script

--- 01cart.sh
+++ 01cart.sh
@@ -64,7 +64,7 @@ VALIDATE $? "extracting cart"
 npm install &>> $LOGFILE
 VALIDATE $? "Installing dependencies"
 
-cp /home/ec2-user/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+cp /home/ec2-user/roboshop_shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
 
 systemctl daemon-reload &>> $LOGFILE
 VALIDATE $? "Daemon reload"
